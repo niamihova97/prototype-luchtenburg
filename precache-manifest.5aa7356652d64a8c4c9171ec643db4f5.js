fc31ff87e0a908552f0abd3afb3fde4d58b68339
julien voccia <julien.voccia@gmail.com>
2020-06-20 18:07:45 +0200

    Updates

---
+++ precache-manifest.5aa7356652d64a8c4c9171ec643db4f5.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a15cf64f8ebdcd5bc8cbce0a7a6e0d5",
+    "url": "/prototype-luchtenburg/index.html"
+  },
+  {
+    "revision": "d55b604bd7d0d6a4c3c2",
+    "url": "/prototype-luchtenburg/static/css/main.629b8818.chunk.css"
+  },
+  {
+    "revision": "802ea40e20540ef19110",
+    "url": "/prototype-luchtenburg/static/js/3.d7f0e6d4.chunk.js"
+  },
+  {
+    "revision": "f3c840f833bd10c66c5b",
+    "url": "/prototype-luchtenburg/static/js/4.9f80f7f0.chunk.js"
+  },
+  {
+    "revision": "83a948c3ac4f025c0114fa02416987e8",
+    "url": "/prototype-luchtenburg/static/js/4.9f80f7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49e2c464ba8abca5450",
+    "url": "/prototype-luchtenburg/static/js/5.86143dd2.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/prototype-luchtenburg/static/js/5.86143dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0dea2b4269275f86c64",
+    "url": "/prototype-luchtenburg/static/js/6.a07a41bf.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/prototype-luchtenburg/static/js/6.a07a41bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55b604bd7d0d6a4c3c2",
+    "url": "/prototype-luchtenburg/static/js/main.1f7c60b3.chunk.js"
+  },
+  {
+    "revision": "1ec47624d66247f7e5c1",
+    "url": "/prototype-luchtenburg/static/js/runtime-main.98cce4d7.js"
+  },
+  {
+    "revision": "65cd2f485d27cf2e8479",
+    "url": "/prototype-luchtenburg/static/js/xlsx.20317a12.chunk.js"
+  },
+  {
+    "revision": "b763177f65cfa2c07ce91b0373c21b0b",
+    "url": "/prototype-luchtenburg/static/media/Competitions.b763177f.png"
+  },
+  {
+    "revision": "29ff31b46eda99e99497595c5a2c3871",
+    "url": "/prototype-luchtenburg/static/media/Daily.29ff31b4.png"
+  },
+  {
+    "revision": "b7198b01696c48cf8cd53a6eea451090",
+    "url": "/prototype-luchtenburg/static/media/EnergySaverCover.b7198b01.png"
+  },
+  {
+    "revision": "4006bed155ea2914807ca3af493f805f",
+    "url": "/prototype-luchtenburg/static/media/Monthly.4006bed1.png"
+  },
+  {
+    "revision": "1841b63fa24a747cb62c1d120c2d5920",
+    "url": "/prototype-luchtenburg/static/media/MyChallenges.1841b63f.png"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/prototype-luchtenburg/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "009fe187fe1d91004663f84a96697a4e",
+    "url": "/prototype-luchtenburg/static/media/Weekly.009fe187.png"
+  },
+  {
+    "revision": "d01903f986f2eee379badf27ed4ea754",
+    "url": "/prototype-luchtenburg/static/media/Yearly.d01903f9.png"
+  },
+  {
+    "revision": "277fd70f43d9294e8d0a3f59994a25fd",
+    "url": "/prototype-luchtenburg/static/media/allAppliances.277fd70f.svg"
+  },
+  {
+    "revision": "bd0e6b90340a13f2001525171e8fe341",
+    "url": "/prototype-luchtenburg/static/media/challenge.bd0e6b90.svg"
+  },
+  {
+    "revision": "07d37d88962001af87d0554d1e17d2a6",
+    "url": "/prototype-luchtenburg/static/media/forest.07d37d88.svg"
+  },
+  {
+    "revision": "a4b95f79a5c14a5f76e386c2d62425ed",
+    "url": "/prototype-luchtenburg/static/media/game_background_1.a4b95f79.png"
+  },
+  {
+    "revision": "e811a4c6a9f3fa4ddcf72f9659b88aac",
+    "url": "/prototype-luchtenburg/static/media/gridIcon.e811a4c6.svg"
+  },
+  {
+    "revision": "ed6b5397512dbdf03acbc0c8c7e4be4e",
+    "url": "/prototype-luchtenburg/static/media/leave.ed6b5397.png"
+  },
+  {
+    "revision": "7fcf6dc4fee43a892b98e049d18ff896",
+    "url": "/prototype-luchtenburg/static/media/littleTree.7fcf6dc4.png"
+  },
+  {
+    "revision": "17ece782177864ef3a3fd24496397700",
+    "url": "/prototype-luchtenburg/static/media/maglass.17ece782.svg"
+  },
+  {
+    "revision": "d85c581b0afc537528dccb30db187dd5",
+    "url": "/prototype-luchtenburg/static/media/myForest.d85c581b.jpg"
+  },
+  {
+    "revision": "84910bbfd140e363b501832008ccc401",
+    "url": "/prototype-luchtenburg/static/media/roomOverview.84910bbf.svg"
+  },
+  {
+    "revision": "08555a2a50e6cbbd4bfa5714ea8706e7",
+    "url": "/prototype-luchtenburg/static/media/settings.08555a2a.svg"
+  },
+  {
+    "revision": "be89f9ae57b0bd8ddf05e395f93f2466",
+    "url": "/prototype-luchtenburg/static/media/trash.be89f9ae.svg"
+  },
+  {
+    "revision": "03a60987487d9425a56c73d336fc9a51",
+    "url": "/prototype-luchtenburg/static/media/trophee.03a60987.png"
+  }
+]);
---
+++ static/js/xlsx.20317a12.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[2],{286:function(p,n){},376:function(p,n){},377:function(p,n){}}]);
+//# sourceMappingURL=xlsx.20317a12.chunk.js.map
